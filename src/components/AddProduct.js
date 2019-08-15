3ffc1c19fb78c5a99a6917f318e34027dc5d8c8b
rjabdurrahman <md.rahman0296@gmail.com>
2019-08-15 13:12:48 +0600

    Added: Add Product route and file

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 export default function AddProduct() {
     return (
-        <div>
-            Add Product
+        <div className="container">
+            <h3>Add Product</h3>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Procut Name</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter product name" />
+                    {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Buying Price</label>
+                    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter buying price" />
+                    {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Selling Price</label>
+                    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter selling price" />
+                    {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Details</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter details" />
+                    {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Image Files</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter file names" />
+                    {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
         </div>
     )
 }
