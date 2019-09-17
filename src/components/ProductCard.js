a221125e91b7dbbea69e98b87552773de232fc3c
rjabdurrahman <md.rahman0296@gmail.com>
2019-09-17 21:33:23 +0600

    Added: Link in category page product

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import './ProductCard.css';
 
 export class ProductCard extends Component {
     render() {
         return (
             <div className="card product-card">
-                <img className="card-img-top mt-4" src={process.env.PUBLIC_URL + '/no_img.jpg'} alt="Har" height={200} />
-                <div className="card-body">
-                    <p className="card-text" style={{ marginBottom: '6px', fontWeight: 500, fontSize: '12px' }}>
-                        {this.props.product.name}</p>
-                    <a href="/dara" className="btn btn-success btn-sm pl-4 pr-4">Buy</a>
-                </div>
+                <Link to={'/product/' + this.props.product.id}>
+
+                    <img className="card-img-top mt-4" src={process.env.PUBLIC_URL + '/no_img.jpg'} alt="Har" height={200} />
+                    <div className="card-body">
+                        <p className="card-text" style={{ marginBottom: '6px', fontWeight: 500, fontSize: '12px' }}>
+                            {this.props.product.name}</p>
+                        <a href="/dara" className="btn btn-success btn-sm pl-4 pr-4">Buy</a>
+                    </div>
+                </Link>
             </div>
         )
     }
